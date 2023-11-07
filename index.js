24b089e140a4aedaa7206301cc0f2c26d89c10ec
iftekahrmahmud98@gmail.com <youremail@example.com>
2023-11-07 12:27:47 +0600

    added wishlist api

--- index.js
+++ index.js
@@ -29,15 +29,11 @@ async function run() {
     const blogsCollection = client.db('ThoughtHere').collection('allBlogs')
     const wishlistCollection = client.db('ThoughtHere').collection('wishlist')
 
-    
-
-
     app.get("/users", async (req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result);
-      });
-  
-      app.post("/users", async (req, res) => {
+      });  
+    app.post("/users", async (req, res) => {
         const user = req.body;
         const query = { email: user.email };
         const existingUser = await usersCollection.findOne(query);
@@ -47,15 +43,11 @@ async function run() {
   
         const result = await usersCollection.insertOne(user);
         res.send(result);
-      });
-
-      
-app.get("/blogs", async (req,res)=> {
+      });     
+    app.get("/blogs", async (req,res)=> {
     const result = await blogsCollection.find().toArray()
     res.send(result)
-})
-
-
+     })
     app.post('/blogs', async (req, res) => {
         
             const blogData = req.body;
@@ -64,7 +56,48 @@ app.get("/blogs", async (req,res)=> {
             res.send(result)
             
         
+     });
+
+     app.post("/wishlist", async (req, res) => {
+        
+          const blog = req.body;
+          blog.user = req.body.user;
+          const result = await wishlistCollection.insertOne(blog);
+          res.send(result)
+     });
+
+     app.delete("/wishlist/:blogTitle", async (req, res) => {
+        try {
+            
+            const blogTitle = req.params.blogTitle;
+            const result = await wishlistCollection.deleteOne({  title: blogTitle });
+    
+            if (result.deletedCount === 1) {
+                res.status(200).json({ message: "Blog removed from wishlist." });
+            } else {
+                res.status(404).json({ error: "Blog not found in the wishlist." });
+            }
+        } catch (error) {
+            console.error("Error removing the blog from the wishlist:", error);
+            res.status(500).json({ error: "Failed to remove the blog from the wishlist" });
+        }
     });
+
+    
+
+      app.get("/wishlist", async (req, res) => {
+        try {
+          const user = req.query.user; 
+          const blogsInWishlist = await wishlistCollection.find({ user }).toArray();
+          res.status(200).json(blogsInWishlist);
+        } catch (error) {
+          console.error("Error fetching blogs from the wishlist:", error);
+          res.status(500).json({ error: "Failed to fetch blogs from the wishlist" });
+        }
+      });
+
+
+    
     
 
   
