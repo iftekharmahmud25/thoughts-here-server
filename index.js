3fd01103b58a081866eb34e9a9f0e2790b84c207
iftekahrmahmud98@gmail.com <youremail@example.com>
2023-11-08 15:06:22 +0600

    deploy vercel

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000; 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin : [
+      'http://localhost:5173',
+        'https://creative-cendol-41505d.netlify.app'
+    ],
+    credentials : true
+}));
 app.use(express.json());
 
 
